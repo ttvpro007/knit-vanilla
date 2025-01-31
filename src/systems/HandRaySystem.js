495f78f9259de1bb5aadea08698e45b3c8b03214
Vi Tiet <vitiet.programmer@gmail.com>
2025-01-31 07:44:00 -0500

    test draggable system

--- src/systems/HandRaySystem.js
+++ src/systems/HandRaySystem.js
@@ -3,62 +3,94 @@ import Object3D from "../components/Object3D.js";
 import { Button } from "../components/Button.js";
 import Intersectable from "../tagcomponents/Intersectable.js";
 
-class HandRaySystem extends System {
+export default class HandRaySystem extends System {
 
     init( attributes ) {
+
         this.handPointers = attributes.handPointers;
+
     }
 
     execute( /*delta, time*/ ) {
+
         this.handPointers.forEach( hp => {
 
             let distance = null;
             let intersectingEntity = null;
             this.queries.intersectable.results.forEach( entity => {
+
                 const object = entity.getComponent( Object3D ).object;
                 const intersections = hp.intersectObject( object, false );
                 if ( intersections && intersections.length > 0 ) {
+
                     if ( distance == null || intersections[ 0 ].distance < distance ) {
+
                         distance = intersections[ 0 ].distance;
                         intersectingEntity = entity;
+
                     }
+
                 }
-            });
+
+            } );
             if ( distance ) {
+
                 hp.setCursor( distance );
                 if ( intersectingEntity.hasComponent( Button ) ) {
+
                     const button = intersectingEntity.getMutableComponent( Button );
                     if ( hp.isPinched() ) {
+
                         button.currState = 'pressed';
+
                     } else if ( button.currState != 'pressed' ) {
+
                         button.currState = 'hovered';
+
                     }
+
                 }
 
                 if ( intersectingEntity.hasComponent( Draggable ) ) {
+
                     const draggable = intersectingEntity.getMutableComponent( Draggable );
                     const object = intersectingEntity.getComponent( Object3D ).object;
                     object.scale.set( 1.1, 1.1, 1.1 );
                     if ( hp.isPinched() ) {
+
                         if ( ! hp.isAttached() && draggable.state != 'attached' ) {
+
                             draggable.state = 'to-be-attached';
                             draggable.attachedPointer = hp;
                             hp.setAttached( true );
+
                         }
+
                     } else {
+
                         if ( hp.isAttached() && draggable.state == 'attached' ) {
+
                             console.log( 'hello' );
                             draggable.state = 'to-be-detached';
                             draggable.attachedPointer = null;
                             hp.setAttached( false );
+
                         }
+
                     }
+
                 }
+
             } else {
+
                 hp.setCursor( 1.5 );
+
             }
-        });
+
+        } );
+
     }
+
 }
 
 HandRaySystem.queries = {
@@ -66,5 +98,3 @@ HandRaySystem.queries = {
         components: [ Intersectable ]
     }
 };
-
-export default HandRaySystem;
